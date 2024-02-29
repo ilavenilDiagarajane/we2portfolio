74ba50232a2d43a1bf11602738848ca9729b3579
ilavenilDiagarajane <venirajane@gmail.com>
2024-02-29 11:57:36 +0530

    homepagechange

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import Home from './Home';
-import Ourcapture from './Ourcapture';
+// import Ourcapture from './Ourcapture';
 function App() {
   return (
-    <div className="container-fluid  app-container">
-      <Home/>
-      {/* <Ourcapture/> */}
+    <div >
+        <Home/> 
+      {/* <Ourcapture/>  */}
       </div>
 
   );
